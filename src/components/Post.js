0569dc8d99b9ceed3f6d8e534c83690f2b7808bf
prosper20 <bobsonconnect@gmail.com>
2024-04-17 17:09:54 +0100

    chore: style post name

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ function Post(props) {
     <li className="post-component">
       <button>
         <img src={props.image} alt={props.name} />
-        <p>{props.name}</p>
+        <p className="post-name">{props.name}</p>
       </button>
     </li>
   );
